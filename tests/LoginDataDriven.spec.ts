0f252a3e6398c02baa0bf18e03323c248362a8f6
Daniele <danielemarinelli1387@gmail.com>
2025-12-19 18:18:22 +0100

    Added implementation in TestDDT.spec.ts

--- tests/LoginDataDriven.spec.ts
+++ tests/LoginDataDriven.spec.ts
@@ -1,6 +1,39 @@
 import {test,expect} from '@playwright/test';
 import {UI_HomePage} from '../pages/HomePage';
 import {TestConfig} from '../test.config';
-import { DataProvider } from '../utils/dataProvider';
+import {DataProvider} from '../utils/dataProvider';
 import {Login} from '../pages/LoginPage';
 import {MyAccount} from '../pages/MyAccountPage';
+
+
+// Load JSON test data from logindata.json file
+const jsonPath:string = "dataset/logindata.json";
+const jsonTestData:any = DataProvider.getDataFromJSON(jsonPath);
+
+for(const data of jsonTestData){
+    test(`Login Test with JSON Data: ${data.testName} @datadriven`, async({page}) =>{
+            const config = new TestConfig();
+            await page.goto(config.appUrl);
+            const hp = new UI_HomePage(page)
+            await hp.myAccountClick();
+            await hp.loginClick()
+
+            const login = new Login(page)
+            await login.performLogin(data.email,data.password)
+
+            //validation
+            if(data.expected.toLowerCase()==='success'){
+                    const myA = new MyAccount(page);
+                    const label:string=await myA.getOrdersLabel();
+                    expect(label).toContain('My Orders')
+            }
+            else{
+                    let errorMsg=await login.getLoginErrorMsg();
+                    expect(errorMsg).toBe('Warning: No match for E-Mail Address and/or Password.')
+            }
+
+    })
+    
+}
+
+
--- utils/dataProvider.ts
+++ utils/dataProvider.ts
@@ -3,18 +3,35 @@ import { parse } from 'csv-parse/sync'
 
 export class DataProvider {
 
-    // method that returns data from JSON file
-    static getTestFromJSON(filePath:string):string{
-    let JsonData:string = JSON.parse(fs.readFileSync(filePath,'utf8'))
-    return JsonData
-}
+    // method that returns data from JSON file 
+    static getDataFromJSON(filePath:string){
+    let JsonData:any = JSON.parse(fs.readFileSync(filePath,'utf8'))
+    return JsonData;
+    }
+
+    /*
+static getDataFromJSON<T>(filePath: string): T {
+    if (!filePath) {
+        throw new Error('File path is required for reading JSON data');
+    }
+    
+    try {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(fileContent) as T;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`Failed to read or parse JSON file: ${error.message}`);
+        }
+        throw error;
+    }
+}  */
 
 
     // method that returns data from CSV file
-    static getTestFromCSV(filePath:string):string{
+    static getDataFromCSV(filePath:string):string{
     let CSVData:any = parse(fs.readFileSync(filePath),{columns:true,skip_empty_lines:true})
     return CSVData
-}
+    }
 
 
 }
